8f31d23dac058ddc2213f3f33cd9cb378dd3df3f
qian li <liq297@163.com>
2018-07-09 20:30:12 +0800

    右边商品栏

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper">
+        <div class="menu-wrapper" v-el:menu-wrapper>
             <ul>
                 <li v-for="item in goods" :key="item.goods" class="menu-item">
                     <span class="text border-1px">
@@ -10,7 +10,7 @@
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper">
+        <div class="foods-wrapper" v-el="food-wrapper">
             <ul>
                 <li v-for="item in goods" :key="item.id" class="food-list">
                     <h1 class="title">{{item.name}}</h1>
@@ -23,12 +23,12 @@
                                 <h2 class="name">{{food.name}}</h2>
                                 <p class="desc">{{food.description}}</p>
                                 <div class="extra">
-                                    <span>月售{{food.sellCount}}份</span>
+                                    <span class="count">月售{{food.sellCount}}份</span>
                                     <span>好评率{{food.rating}}%</span>
                                 </div>
                                 <div class="price">
-                                    <span>￥{{food.price}}</span>
-                                    <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                                    <span class="now">￥{{food.price}}</span>
+                                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                                 </div>
                             </div>
                         </li>
@@ -39,6 +39,7 @@
     </div>
 </template>
 <script type='text/ecmascript-6'>
+    import BScroll from "better-scroll";
     const ERR_OK = 0;
     export default{
         data(){
@@ -55,8 +56,17 @@
             this.classMap = ['decrease','discount','special','invoice','guarantee'];
             this.$http.get('/api/goods').then((res)=>{
                 var response = res.body;
-                this.goods = response
+                this.goods = response;
+                this.$nextTick(()=>{
+                    this._initScroll();
+                });
             });
+        },
+        methods:{
+            _initScroll(){
+                this.menuScroll = new BScroll(this.$els.menuWrapper,{});
+                this.foodScroll = new BScroll(this.$els.foodWrapper,{});
+            }
         }
     };
 </script>
@@ -72,9 +82,9 @@
         .foods-wrapper
             flex 1
             .title
-                padding-left 28px
+                padding-left 14px
                 height 26px
-                line-height 36px
+                line-height 26px
                 border-left 2px solid #d9dde1
                 font-size 12px
                 color rgb(147,153,159)
@@ -83,9 +93,43 @@
             .food-item
                 display flex
                 margin 18px 
+                padding-bottom 18px
                 border-1px(rgba(7,17,27,0.1))
                 &:last-child
                     border-none()
+                    margin-bottom 0
+                .icon
+                    flex 0 0 57px
+                    margin-right 10px
+                .content
+                    flex 1
+                    .name
+                        margin 2px 0 8px 0
+                        height 14px
+                        line-height 14px
+                        font-size 14px
+                        color rgb(7,17,27)
+                    .desc,.extra
+                        line-height 10px
+                        font-size 10px
+                        color rgb(147,153,159)
+                    .desc
+                        margin-bottom 8px
+                    .extra
+                        &.count
+                            margin-right 12px
+                    .price
+                        font-weight 700
+                        line-height 24px
+                        .now
+                            margin-right 8px
+                            font-size 14px
+                            color rgb(240,20,20)
+                        .old
+                            text-decoration line-through
+                            font-size 10px
+                            color rgb(147,153,159)
+
         .menu-wrapper
             flex 0 0 80px
             width 80px
